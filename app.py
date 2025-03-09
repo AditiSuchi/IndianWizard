36e5515e6953bc3b52bdbb1efe1cdae2bf1e38e5
Baiju Kuamr Yadav <yadavbaiju2005@gmail.com>
2025-03-09 12:01:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Serve sitemap.xml
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory("static", "sitemap.xml")
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory("static", "robots.txt")
 
 if __name__ == '__main__':
     app.run(debug=True)
